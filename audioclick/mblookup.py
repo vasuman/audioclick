52029a6ab6ef3106b7020ff0966851652ce49234
Vasuman <vasumanar@gmail.com>
2012-07-22 14:15:23 +0530

    Removed older track selection function

--- audioclick/mblookup.py
+++ audioclick/mblookup.py
@@ -4,42 +4,16 @@ import musicbrainzngs as mb
 
 mb.set_useragent('AudioClick','0.1dev')
 
-def request_release(releases):
-	num=1
-	if len(releases)==1 : return releases[0]
-	print 'The track has been released in these albums:-'
-	for release in releases:
-		if 'date' in release:
-			print str(num)+'.\t'+release['title']+'\t'+release['date']
-			num+=1
-			continue
-		print str(num)+'.\t'+release['title']
-		num+=1
-	opt=int(raw_input('Enter the album no.:'))
-	return releases[opt]
-
 def extract_year(release):
 	if 'date' in release.keys() : 
 		year=int(release['date'].replace('-','')[:4])
 	return -1
 
-def single_recording_lookup(mbids, func=request_release):
-	mbid=mbids[0]
-	rec_info=mb.get_recording_by_id(mbid,['artists','releases'],['official'])['recording']
-	track={}
-	track['title']=rec_info['title']
-	track['artist']=rec_info['artist-credit-phrase']
-	track['musicbrainz_trackid']=mbid
-	releases=rec_info['release-list']
-	print track['artist']+' - '+track['title']							#Print in request func!
-	if len(releases)==1 :
-		final_release=releases[0]
-	else :
-		final_release=func(releases)
-	track['album']=final_release['title']
-	track['date']=final_release['date']
-	return track
-
+def find_oldest_release(releases):
+	releases.sort(key=extract_year)
+	for release in releases:
+		if 'date' in release.keys() :
+			return release
 def generate_track(rec_info,final_release):
 	track={}
 	track['title']=rec_info['title']
@@ -67,10 +41,5 @@ def match_recordings(mbids):
 	oldest_rec=mbid_rec[oldest_release['trackid']]
 	return generate_track(oldest_rec,oldest_release)
 
-def find_oldest_release(releases):
-	releases.sort(key=extract_year)
-	for release in releases:
-		if 'date' in release.keys() :
-			return release
 	
 #def multiple_track_match(mbids):	
